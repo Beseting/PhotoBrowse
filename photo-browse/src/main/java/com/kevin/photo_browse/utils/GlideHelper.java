9b8fb366bf44be5363f7cc4b4ad5a7ceed0a3776
Kevin <970540337@qq.com>
2017-10-10 23:11:12 +0800

    优化GlideHelper

--- photo-browse/src/main/java/com/kevin/photo_browse/utils/GlideHelper.java
+++ photo-browse/src/main/java/com/kevin/photo_browse/utils/GlideHelper.java
@@ -1,6 +1,7 @@
 package com.kevin.photo_browse.utils;
 
 import android.app.Activity;
+import android.content.Context;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -12,6 +13,13 @@ import com.kevin.photo_browse.main.GlideApp;
  */
 
 public final class GlideHelper {
+    public static void load(Context context, Object url, ImageView imageView){
+        GlideApp.with(context)
+                .load(url)
+                .placeholder(R.drawable.img_placeholder)
+                .error(R.drawable.img_error)
+                .into(imageView);
+    }
     public static void load(Activity activity,Object url,ImageView imageView){
         load(activity,url,imageView,true);
     }
